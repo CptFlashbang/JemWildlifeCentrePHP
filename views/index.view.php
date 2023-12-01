897a7564feb1c5b7cea385826bbfaa4acf74c276
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-01 23:05:15 +0000

    Update index.view.php
    Added content placeholders to index

--- views/index.view.php
+++ views/index.view.php
@@ -3,9 +3,57 @@ require('partials/head.php');
 require('partials/header.php');
 ?>
 <main>
-    <div class="mx-auto max-w-5xl py-6 px-8">
-        <p>Hello. Welcome to the home page.</p>
-    </div>
+    <section>
+        <p>Welcome, to the</p>
+        <p>Jem Wildlife Centre</p>
+        <p>Nurturing nature, one rescue at a time</p>
+        <p>enquiries@JemWildlifeCentre.org </p>
+        <p>(+44) 07443412378 </p>
+        <div>
+            <p>See our work</p>
+            <p>(Down arrow)</p>
+        </div>
+    </section>
+    <br>   
+    <section>
+        <p>We have had the privilege of collaborating with these incredible conservation partners. </p>
+        <p>PARTNER 1</p>
+        <p>PARTNER 2</p>
+        <p>PARTNER 3</p>
+        <p>PARTNER 4</p>
+        <p>PARTNER 5</p>
+    </section>
+    <br>   
+    <section>
+        <p>Our work</p>
+        <p>Our mission is to help sick, injured and orphaned wildlife, either by rehabilitating and returning them back into the wild, or by providing a life-long home to those who would not survive in the wild.</p>
+        <p>IMAGE</p>
+        <div>
+            <p>“Partnering with the Jem Wildlife Centre has been an absolute delight, and we eagerly anticipate engaging their expertise in our shared commitment to wildlife conservation.”</p>
+            <p>IMAGE</p>
+            <p>John Smith</p>            
+            <p>WWF</p>
+        </div>
+    </section>
+    <br> 
+    <section>
+        <p>Our residents</p>
+        <p>Meet a few of our incredible residents! On our homepage, you'll catch a glimpse of three remarkable animals, each with its own unique story and charm. These featured creatures represent just a fraction of the diverse wildlife thriving at Jem Wildlife Centre. Curious to discover more? Click the link to explore our full roster of fascinating animals, each contributing to the rich tapestry of life we cherish and protect in our sanctuary.</p>
+        <p>SEE MORE BUTTON</p>
+        <p>IMAGE 1</p>
+        <p>IMAGE 2</p>
+        <p>IMAGE 3</p>
+    </section>
+    <br> 
+    <section>
+        <p>Events</p>
+        <p>Explore the excitement awaiting you at Jem Wildlife Centre! Right on our homepage, we spotlight three upcoming events designed to cater to diverse interests and age groups. Whether it's an exclusive gathering for adults, a fun-filled experience for families, or an engaging adventure tailored for children, our events promise something special for everyone. </p>
+        <p>Intrigued? Dive into the details and mark your calendar for a memorable experience. Click the link below to discover the full spectrum of events we have in store, each crafted with a touch of wonder and a commitment to wildlife education and enjoyment.</p>
+        <p>SEE MORE BUTTON</p>
+        <p>IMAGE 1</p>
+        <p>IMAGE 2</p>
+        <p>IMAGE 3</p>
+    </section>
 </main>
 <?php
 require('partials/banner.php');
