67efda5432ee710c9cfc8ca049b3d20788ab3957
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-01 17:19:41 +0000

    Updated nav.php

---
+++ views/partials/nav.php
@@ -0,0 +1,19 @@
+<nav>
+  <ul class="p-0 m-0 flex bg-orange-700 flex-row flex-wrap">
+    <li class="grow p-0 w-1/5 border border-black border-solid box-border">
+      <a href="/JemWildlifeCentrePHP/" class="leading-[3] min-h-[45px] block text-slate-50 text-center" <?= urlIs('/JemWildlifeCentrePHP/') ? 'aria-current="page"': '' ?>>Home</a>
+    </li>
+    <li class="grow p-0 w-1/5 border border-black border-solid box-border">
+      <a href="/JemWildlifeCentrePHP/about" class="leading-[3] min-h-[45px] block text-slate-50 text-center" <?= urlIs('/JemWildlifeCentrePHP/about') ? 'aria-current="page"': '' ?>>About</a>
+    </li>
+    <li class="grow p-0 w-1/5 border border-black border-solid box-border">
+      <a href="/JemWildlifeCentrePHP/about" class="leading-[3] min-h-[45px] block text-slate-50 text-center" <?= urlIs('/JemWildlifeCentrePHP/animals') ? 'aria-current="page"': '' ?>>About</a>
+    </li>
+    <li class="grow p-0 w-1/5 border border-black border-solid box-border">
+      <a href="/JemWildlifeCentrePHP/about" class="leading-[3] min-h-[45px] block text-slate-50 text-center" <?= urlIs('/JemWildlifeCentrePHP/events') ? 'aria-current="page"': '' ?>>About</a>
+    </li>
+    <li class="grow p-0 w-1/5 border border-black border-solid box-border">
+      <a href="/JemWildlifeCentrePHP/about" class="leading-[3] min-h-[45px] block text-slate-50 text-center" <?= urlIs('/JemWildlifeCentrePHP/contact') ? 'aria-current="page"': '' ?>>About</a>
+    </li>
+  </ul>
+</nav>
