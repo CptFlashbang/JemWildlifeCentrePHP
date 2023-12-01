80142e19c7c79219ac4816e5333c60750e2ee557
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-01 17:23:23 +0000

    Update nav.php
    Fixed bad labels on nav

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -7,13 +7,13 @@
       <a href="/JemWildlifeCentrePHP/about" class="leading-[3] min-h-[45px] block text-slate-50 text-center" <?= urlIs('/JemWildlifeCentrePHP/about') ? 'aria-current="page"': '' ?>>About</a>
     </li>
     <li class="grow p-0 w-1/5 border border-black border-solid box-border">
-      <a href="/JemWildlifeCentrePHP/animals" class="leading-[3] min-h-[45px] block text-slate-50 text-center" <?= urlIs('/JemWildlifeCentrePHP/animals') ? 'aria-current="page"': '' ?>>About</a>
+      <a href="/JemWildlifeCentrePHP/animals" class="leading-[3] min-h-[45px] block text-slate-50 text-center" <?= urlIs('/JemWildlifeCentrePHP/animals') ? 'aria-current="page"': '' ?>>Animals</a>
     </li>
     <li class="grow p-0 w-1/5 border border-black border-solid box-border">
-      <a href="/JemWildlifeCentrePHP/events" class="leading-[3] min-h-[45px] block text-slate-50 text-center" <?= urlIs('/JemWildlifeCentrePHP/events') ? 'aria-current="page"': '' ?>>About</a>
+      <a href="/JemWildlifeCentrePHP/events" class="leading-[3] min-h-[45px] block text-slate-50 text-center" <?= urlIs('/JemWildlifeCentrePHP/events') ? 'aria-current="page"': '' ?>>Events</a>
     </li>
     <li class="grow p-0 w-1/5 border border-black border-solid box-border">
-      <a href="/JemWildlifeCentrePHP/contact" class="leading-[3] min-h-[45px] block text-slate-50 text-center" <?= urlIs('/JemWildlifeCentrePHP/contact') ? 'aria-current="page"': '' ?>>About</a>
+      <a href="/JemWildlifeCentrePHP/contact" class="leading-[3] min-h-[45px] block text-slate-50 text-center" <?= urlIs('/JemWildlifeCentrePHP/contact') ? 'aria-current="page"': '' ?>>Contact</a>
     </li>
   </ul>
 </nav>
