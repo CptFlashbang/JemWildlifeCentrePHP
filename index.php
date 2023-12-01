98a1274d89685b8d10f046c35b6a413362caf7ae
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-01 17:20:48 +0000

    Update index.php
    Testing partials

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="output.css">
-    <title>Document</title>
-</head>
-<body>
-    <p>Index Page</p>
-</body>
-</html>
+<?php
+$heading = "Home";
+require 'functions.php';
+require('views/partials/head.php');
+require('views/partials/nav.php');
+require('views/partials/banner.php');
+?>
+<main>
+    <div class="mx-auto max-w-5xl py-6 px-8">
+        <p>Hello World!</p>
+    </div>
+</main>
+
+<?php
+require('views/partials/footer.php');
+?>
