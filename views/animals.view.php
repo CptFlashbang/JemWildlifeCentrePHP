233bd8be574b8eff974e53f6680063fa0a169e9b
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-01 18:59:22 +0000

    Updated views
    Added partials and basic message to view of each view

---
+++ views/animals.view.php
@@ -0,0 +1,13 @@
+<?php
+require('partials/head.php');
+require('partials/nav.php');
+require('partials/banner.php');
+?>
+<main>
+    <div class="mx-auto max-w-5xl py-6 px-8">
+        <p>Hello. Welcome to the animals page.</p>
+    </div>
+</main>
+<?php
+require('partials/footer.php');
+?>
--- views/index.view.php
+++ views/index.view.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="output.css">
-    <title>Document</title>
-</head>
-<body>
-    <p>Index Page</p>
-</body>
-</html>
+<?php
+require('partials/head.php');
+require('partials/nav.php');
+require('partials/banner.php');
+?>
+<main>
+    <div class="mx-auto max-w-5xl py-6 px-8">
+        <p>Hello. Welcome to the home page.</p>
+    </div>
+</main>
+<?php
+require('partials/footer.php');
+?>
