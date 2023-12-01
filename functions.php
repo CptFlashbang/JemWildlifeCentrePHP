9e9ec5b9897061c64450920c59f85fb69c8fd847
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-01 22:45:10 +0000

    Updated banner.php

--- functions.php
+++ functions.php
@@ -3,4 +3,3 @@
 function urlIs($value) {
     return parse_url($_SERVER['REQUEST_URI'])['path'] === $value;
 }
-
--- views/partials/banner.php
+++ views/partials/banner.php
@@ -1,5 +1,11 @@
 <header class="bg-white shadow">
   <div class="mx-auto max-w-7xl py-6 px-8">
-    <h1 class="text-3xl font-bold tracking-tight text-gray-900"><?=$heading?></h1>
+    <p>Join us in making a positive impact on wildlife. </p>
+    <p>If you're passionate about wildlife conservation and eager to contribute, let's work together. </p>
+    <p>Have a wildlife project in mind that could use our support? Let’s talk! </p>
+  </div>
+  <div class="mx-auto max-w-7xl py-6 px-8">
+    <p>enquiries@JemWildlifeCentre.org </p>
+    <p>(+44) 07443412378 </p>
   </div>
 </header>
