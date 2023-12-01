5cf6fa6c23c8787da7cdd799cfd80c4f8da7bb81
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-01 19:31:25 +0000

    Update socials.php
    Added basic list of socials

---
+++ views/partials/socials.php
@@ -0,0 +1,16 @@
+<div>
+    <ul>
+        <li>
+            <p>Facebook</p>
+        </li>
+        <li>
+            <p>Instagram</p>
+        </li>
+        <li>
+            <p>Twitter</p>
+        </li>
+        <li>
+            <p>Tiktok</p>
+        </li>
+    </ul>
+</div>
