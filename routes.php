b727904dded92439db037e2b25f29ea6fead7480
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-01 19:04:23 +0000

    Update routes.php
    Added routes to all pages

---
+++ routes.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    '/JemWildlifeCentrePHP/' => 'controllers/index.php',
+    '/JemWildlifeCentrePHP/about' => 'controllers/about.php',
+    '/JemWildlifeCentrePHP/animals' => 'controllers/animals.php',
+    '/JemWildlifeCentrePHP/events' => 'controllers/events.php',
+    '/JemWildlifeCentrePHP/contact' => 'controllers/contact.php'
+];
