578348ddc7ebc7b158f6913faf27f5c0de28d7f0
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-01 19:55:14 +0000

    Update footer.php
    Added footer content

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,5 +1,19 @@
         <footer>
             &copy; <?=date("Y")?> Callum Yates
+            <div>
+                <ul>
+                    <li>
+                        <p>Privacy Policy</p>
+                    </li>
+                    <li>
+                        <p>Terms and Conditions</p>
+                    </li>
+                </ul>
+            </div>
+            
+            <?php
+            require('socials.php');
+            ?>
         </footer>
     </body>
 </html>
