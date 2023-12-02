369117b61c7fcbd18346bbdabdab84f45223be41
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-02 00:05:08 +0000

    Updated header.php
    Added css to header.php to make each section 1/3 screen width

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,7 +1,19 @@
-<header>
-    <p>LOGO</p>
-    <?php
-        require('nav.php');
-        require('socials.php');
-    ?>
+<header class = "flex">
+    <div class="w-1/3 bg-red-200">
+        <!-- <p>RED</p> -->
+        <p>LOGO</p>
+        
+    </div>
+    <div class="w-1/3 bg-green-200">
+        <!-- <p>GREEN</p> -->
+        <?php
+            require('nav.php');
+        ?>
+    </div>
+    <div class="w-1/3 bg-blue-200">
+        <!-- <p>BLUE</p> -->
+        <?php
+            require('socials.php');
+        ?>
+    </div>
 </header>
