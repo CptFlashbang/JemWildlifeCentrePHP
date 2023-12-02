42f372ec08968061ddfc79bdcacd74cdbf9766f2
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-02 02:38:20 +0000

    Updated index.view.php
    Added CSS to index.view.php

--- views/partials/header.php
+++ views/partials/header.php
@@ -1,10 +1,9 @@
-<header class = "flex flex-row">
+<header class = "h-20 items-center flex flex-row border-b-2 border-black bg-blue-400">
     <div class="w-1/3 bg-red-200">
         <!-- <p>RED</p> -->
-        <p>LOGO</p>
-        
+        <p>LOGO</p>        
     </div>
-    <div class="w-1/3 bg-green-200">
+    <div class="w-1/3  bg-green-200">
         <!-- <p>GREEN</p> -->
         <?php
             require('nav.php');
