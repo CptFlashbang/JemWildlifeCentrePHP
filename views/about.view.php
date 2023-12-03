f03cf9768372490b1978f731b12346c382618c27
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-03 13:46:36 +0000

    Updated about.view.php
    Added styling for top section

--- views/about.view.php
+++ views/about.view.php
@@ -3,41 +3,40 @@ require('partials/head.php');
 require('partials/header.php');
 ?>
 <main>
-    <section>
-        <div class="mx-auto max-w-5xl py-6 px-8">
-            <p>We are the Jem wildlife centre</p>
-            <p>Nestled near Penkridge in Staffordshire, Jem Wildlife Centre is a refuge for British wildlife in need. Dedicated to the rescue and care of sick, injured, and orphaned animals, our mission is woven into every pawprint and feathered wing.</p>
-            <p>From the resilient European Hedgehogs to the graceful strides of Red Foxes, each resident embodies the spirit of our commitment. Inhabitants like Roe Deer, alongside the harmonious presence of Common Toads, and the majestic grace of Red Deer, find solace within our sanctuary.</p>
-            <p>Our haven extends to the skies, where the majestic presence of Barn Owls and the melodious songs of Skylarks echo our dedication. Join us in safeguarding the wonders of the wild. Jem Wildlife Centre: A refuge for compassion, conservation, and the love of nature.</p>
+    <section class="border-b-2 border-black">
+        <div class="grid grid-cols-5 grid-rows-6">
+            <h1 class="col-start-2 col-end-3 row-start-1 row-end-2 mt-32">We are the Jem wildlife centre</h1>
+            <p class="col-start-2 col-end-3 row-start-2 row-end-3">Nestled near Penkridge in Staffordshire, Jem Wildlife Centre is a refuge for British wildlife in need. Dedicated to the rescue and care of sick, injured, and orphaned animals, our mission is woven into every pawprint and feathered wing.</p>
+            <p class="col-start-2 col-end-3 row-start-3 row-end-4 my-auto">From the resilient European Hedgehogs to the graceful strides of Red Foxes, each resident embodies the spirit of our commitment. Inhabitants like Roe Deer, alongside the harmonious presence of Common Toads, and the majestic grace of Red Deer, find solace within our sanctuary.</p>
+            <p class="col-start-2 col-end-3 row-start-4 row-end-5 my-auto">Our haven extends to the skies, where the majestic presence of Barn Owls and the melodious songs of Skylarks echo our dedication. Join us in safeguarding the wonders of the wild. Jem Wildlife Centre: A refuge for compassion, conservation, and the love of nature.</p>        
+            <p class="col-start-4 col-end-5 row-start-4 row-end-5 my-auto">SEE MORE BUTTON</p>
+            <div class="flex flex-row mt-20 mb-32 col-start-2 col-end-5 row-start-5 row-end-6 mx-auto">
+                <p class="mx-12">IMAGE 1</p>
+                <p class="mx-12">IMAGE 2</p>
+                <p class="mx-12">IMAGE 3</p>
+            </div>
         </div>
-        <div class="mx-auto max-w-5xl py-6 px-8">
-            <p>IMAGE 1</p>
-            <p>IMAGE 2</p>
-            <p>IMAGE 3</p>
-        </div>
-        <div class="mx-auto max-w-5xl py-6 px-8">
-            <p>Our core values:</p>
-            <div class="mx-auto max-w-5xl py-6 px-8">
-                <div class="mx-auto max-w-5xl py-6 px-8">
-                    <p>Compassion</p>
-                    <p>We are driven by a deep empathy for every living being, fostering compassion in our actions and decisions.</p>
-                </div>
-                <div class="mx-auto max-w-5xl py-6 px-8">
-                    <p>Dedication to Rehabilitation</p>
-                    <p>We are committed to the rehabilitation and well-being of sick, injured, and orphaned wildlife, striving to give each animal a chance for a healthy and fulfilling life.</p>
-                </div>
-                <div class="mx-auto max-w-5xl py-6 px-8">
-                    <p>Respect for Nature</p>
-                    <p>We recognize and honor the intrinsic value of every creature and the natural world, promoting respect for biodiversity and ecosystems.</p>
-                </div>
-                <div class="mx-auto max-w-5xl py-6 px-8">
-                    <p>Dedication to Rehabilitation</p>
-                    <p>We actively engage with our local community, building partnerships and collaborations that promote a shared commitment to wildlife conservation.</p>
-                </div>
+        <div class="mx-auto max-w-5xl py-6 px-8 grid grid-cols-2 grid-rows-4">
+            <h2 class="col-start-1 col-end-3 row-start-1 row-end-2">Our core values:</h2>
+            <div class="col-start-1 col-end-2 row-start-2 row-end-3  mx-auto max-w-5xl py-6 px-8 ">
+                <h3>Compassion</h3>
+                <p>We are driven by a deep empathy for every living being, fostering compassion in our actions and decisions.</p>
+            </div>
+            <div class="col-start-2 col-end-3 row-start-2 row-end-3 mx-auto max-w-5xl py-6 px-8">
+                <h3>Dedication to Rehabilitation</h3>
+                <p>We are committed to the rehabilitation and well-being of sick, injured, and orphaned wildlife, striving to give each animal a chance for a healthy and fulfilling life.</p>
+            </div>
+            <div class="col-start-1 col-end-2 row-start-3 row-end-4 mx-auto max-w-5xl py-6 px-8">
+                <h3>Respect for Nature</h3>
+                <p>We recognize and honor the intrinsic value of every creature and the natural world, promoting respect for biodiversity and ecosystems.</p>
+            </div>
+            <div class="col-start-2 col-end-3 row-start-3 row-end-4 mx-auto max-w-5xl py-6 px-8">
+                <h3>Dedication to Rehabilitation</h3>
+                <p>We actively engage with our local community, building partnerships and collaborations that promote a shared commitment to wildlife conservation.</p>
             </div>
         </div>
     </section>
-    <br>
+    
     <section>
         <div>
             <div>
