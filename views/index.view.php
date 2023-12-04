11951d4d76e62f9c7a5597d5afb0a1301076263e
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-04 00:05:55 +0000

    Reverted section.
    Reverted section to individual, working with blocks here would not work. Different format would require edit of blocks.php

--- views/index.view.php
+++ views/index.view.php
@@ -34,7 +34,7 @@ require('partials/blocks.php');
     <section class= "justify-center items-center border-b-2 border-black bg-white mx-auto">
         <div class="mx-auto max-w-screen-sm flex flex-col items-center mt-20 mb-20">
             <h1>Our work</h1>
-            <p class="text-center">Our mission is to help sick, injured and orphaned wildlife, either by rehabilitating and returning them back into the wild, or by providing a life-long home to those who would not survive in the wild.</p>
+            <p class="text-center py-8">Our mission is to help sick, injured and orphaned wildlife, either by rehabilitating and returning them back into the wild, or by providing a life-long home to those who would not survive in the wild.</p>
         </div>
         <img src="Images\Seal.jpg" class="mx-auto">
         <div class="w-3/5 mx-auto max-w-screen-sm flex flex-col items-center mt-40 mb-16">
