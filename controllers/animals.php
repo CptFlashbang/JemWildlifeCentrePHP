da8e6ae657fb8068e212ec9cd6e181ee393815d4
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-04 16:41:46 +0000

    Added config.php

--- controllers/animals.php
+++ controllers/animals.php
@@ -1,3 +1,15 @@
 <?php
 $heading = "Animals";
 require "views/animals.view.php";
+
+
+require 'functions.php';
+$config = require('config.php');
+$dsn = 'mysql:' . http_build_query($config['database'], '', ';');
+$pdo = new PDO($dsn, 'root', '');
+$statement = $pdo->prepare("select * from books");
+$statement->execute();
+$books = $statement->fetchAll(PDO::FETCH_ASSOC);
+dd($books);
+
+require 'router.php';
