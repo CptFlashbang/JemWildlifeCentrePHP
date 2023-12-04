9e009bfc6861ad290a16315f717b42975499f02d
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-04 02:38:48 +0000

    Update contact.view.php
    Added question and answer data sets (placeholders), and a function call to blocks.php

--- views/contact.view.php
+++ views/contact.view.php
@@ -7,7 +7,26 @@ require('partials/blocks.php');
 <main>
     <div class="mx-auto max-w-5xl py-6 px-8">
         <p>Hello. Welcome to the contact page.</p>
-    </div>
+    </div>    
+    <?php
+        $questions = [
+            "q1" => 
+            "When was Halestorm formed, and who are the founding members?",
+            "q2" => 
+            "What is Halestorm's breakthrough album, and what notable achievements did it attain?",
+        ];
+        
+        $answers = [
+            "q1" => 
+            ['text' => 'Halestorm was formed in 1997 in Red Lion, Pennsylvania, USA. The founding members are siblings Lzzy Hale (vocals, guitar, and keyboard) and Arejay Hale.',
+             'status' => 'Visible'],
+            "q2" =>
+            ['text' => 'Halestorm\'s breakthrough album is their self-titled release, "Halestorm," which came out in 2009. The album features hit tracks like "I Get Off" and "It\'s Not You" and earned the band a Grammy Award for Best Hard Rock/Metal Performance for the song "Love Bites (So Do I)" in 2013.',
+             'status' => 'Visible'],
+
+        ];
+    generateBlockFAQ($questions, $answers)
+    ?>
 </main>
 <?php
 require('partials/footer.php');
