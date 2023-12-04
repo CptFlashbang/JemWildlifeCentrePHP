1203418efbf27f82b2849e0d25796c68652560a1
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-04 18:16:56 +0000

    Updated animals.php
    Added query to retrieve all animal information from database

--- controllers/animals.php
+++ controllers/animals.php
@@ -1,15 +1,22 @@
 <?php
 $heading = "Animals";
-require "views/animals.view.php";
-
-
-require 'functions.php';
 $config = require('config.php');
 $dsn = 'mysql:' . http_build_query($config['database'], '', ';');
 $pdo = new PDO($dsn, 'root', '');
-$statement = $pdo->prepare("select * from books");
+$statement = $pdo->prepare("
+SELECT
+    A.*,
+    S.Genus,
+    S.Species,
+    S.Common_Name,
+    CS.Conservation_Status_Name
+FROM
+    Animal A
+JOIN
+    Species_Information S ON A.Species_ID_FK = S.Species_ID_PK
+JOIN
+    Conservation_Status CS ON S.Conservation_Status_FK = CS.Conservation_Status_PK
+");
 $statement->execute();
-$books = $statement->fetchAll(PDO::FETCH_ASSOC);
-dd($books);
-
-require 'router.php';
+$animals = $statement->fetchAll(PDO::FETCH_ASSOC);
+require "views/animals.view.php";
--- views/animals.view.php
+++ views/animals.view.php
@@ -6,6 +6,14 @@ require('partials/header.php');
     <div class="mx-auto max-w-5xl py-6 px-8">
         <p>Hello. Welcome to the animals page.</p>
     </div>
+    <div>
+        <ul class="my-3">
+            <?php
+            foreach ($animals as $animal) {
+                echo "<li><strong>" . $animal['Name'] . "</strong> by <span>" . $animal['Sex'] . "</span> with " . $animal['General_Appearance'] ." pages</li>";
+            } ?>
+        </ul>
+    </div>
     <?php    
     require('partials/banner.php');
     ?>
