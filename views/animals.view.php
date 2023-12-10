546ad9c342629e01262dd39c427d21ac393edc6e
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-10 22:44:14 +0000

    Added looping box creation

--- views/animals.view.php
+++ views/animals.view.php
@@ -6,14 +6,38 @@ require('partials/header.php');
     <div class="mx-auto max-w-5xl py-6 px-8">
         <p>Hello. Welcome to the animals page.</p>
     </div>
-    <div>
+
+    <div class="bg-red-700">
+        <p>PICTURE</p>
+        <p>NAME</p>
+        <p>SEX</p>
+        <p>General_Appearance</p>
+    </div>
+
+    
+    <?php
+        foreach ($animals as $animal) {
+            echo (
+                "<div class=\"bg-blue-700\">" .
+                    "<p>PICTURE</p>" . 
+                    "<p>".$animal['Name']."</p>" . 
+                    "<p>".$animal['Sex']."</p>" . 
+                    "<p>General_Appearance</p>".
+                "</div>"
+            );
+        } 
+    ?>
+
+    
+
+    <!-- <div>
         <ul class="my-3">
             <?php
-            foreach ($animals as $animal) {
-                echo "<li><strong>" . $animal['Name'] . "</strong> by <span>" . $animal['Sex'] . "</span> with " . $animal['General_Appearance'] ." pages</li>";
-            } ?>
+            // foreach ($animals as $animal) {
+            //     echo "<li><strong>" . $animal['Name'] . "</strong> by <span>" . $animal['Sex'] . "</span> with " . $animal['General_Appearance'] ." pages</li>"} 
+            ?>
         </ul>
-    </div>
+    </div> -->
     <?php    
     require('partials/banner.php');
     ?>
