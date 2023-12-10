d43b6f57706b1333cebcd6e2a060331d27f9b766
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-12-10 13:22:45 +0000

    Update about.view.php
    Changed from procedural block generation to static HTML

--- views/about.view.php
+++ views/about.view.php
@@ -48,14 +48,11 @@ require('partials/blocks.php');
                 <p>WWF</p>';
             </div>   
             <div class="w-3/5 mx-auto max-w-screen-sm flex flex-col items-center mt-40 mb-16 border-2 border-black">
-                <?php
-                $quote = "Partnering with the Jem Wildlife Centre has been an absolute delight, and we eagerly anticipate engaging their expertise in our shared commitment to wildlife conservation.";
-                $author= "John Smith";
-                $organisation= "WWF";
-                $image1 = "";
-                $image2 = "";
-                generateBlockTestimonial($quote, $author, $organisation)
-                ?>
+                <p class="text-center text-xl">Partnering with the Jem Wildlife Centre has been an absolute delight, and we eagerly anticipate engaging their expertise in our shared commitment to wildlife conservation.</p>';
+                <p>IMAGE</p>';
+                <p>IMAGE</p>';
+                <p class="font-bold">John Smith</p>';
+                <p>WWF</p>';
             </div>      
         </div>
         <div class="flex flex-row">
