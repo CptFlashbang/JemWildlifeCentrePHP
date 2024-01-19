5e172dfe8018d966983a8ee457ddd7a7e7a018ac
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-19 21:48:12 +0000

    Update index.php
    Added query to retrieve count of animals

--- controllers/index.php
+++ controllers/index.php
@@ -1,3 +1,12 @@
 <?php
 $heading = "Home";
+
+$config = require('config.php');
+$db = new Database($config['database']);
+$result = $db->query('
+SELECT COUNT(*) AS total_row_count
+FROM animal;
+')->findOrFail();
+
+
 require "views/index.view.php";
