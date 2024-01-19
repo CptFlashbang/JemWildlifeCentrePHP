4772fb711fa9ee422afbcca42e3ce082fe4d4c57
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-19 15:47:07 +0000

    Update animal_detail.php
    Updated SQL with better readability version

--- controllers/animal_detail.php
+++ controllers/animal_detail.php
@@ -1,11 +1,11 @@
-<?php
-$heading = "Chosen Book " . $book['title'] . " with ID: " . htmlspecialchars($_GET['id']);
-$config = require('config.php');
-$db = new Database($config['database']);
-$book = $db->query('select * from animals where ID = :id', ['id' => $_GET['id']])->findOrFail();
-$returnURL = $_GET['return'];
-require "views/book.view.php";
-?>
+<!-- <?php
+// $heading = "Chosen Book " . $book['title'] . " with ID: " . htmlspecialchars($_GET['id']);
+// $config = require('config.php');
+// $db = new Database($config['database']);
+// $book = $db->query('select * from animals where ID = :id', ['id' => $_GET['id']])->findOrFail();
+// $returnURL = $_GET['return'];
+// require "views/book.view.php";
+?> -->
 
 <?php
 $heading = "Animal Detail". $animal['title'] . " with ID: " . htmlspecialchars($_GET['id']);
@@ -19,9 +19,10 @@ SELECT
     Species_Information.Species,
     Species_Information.Common_Name,
     Conservation_Status.Conservation_Status_Name
-FROM Animal
-JOIN Species_Information ON Species_ID_FK = Species_ID_PK
-JOIN Conservation_Status ON Conservation_Status_FK = Conservation_Status_PK
+FROM
+    Animal
+JOIN Species_Information ON Animal.Species_ID_FK = Species_Information.Species_ID_PK
+JOIN Conservation_Status ON Species_Information.Conservation_Status_FK = Conservation_Status.Conservation_Status_PK
 WHERE
     Animal.ID = :id
 ', ['id' => $_GET['id']])->findOrFail();
