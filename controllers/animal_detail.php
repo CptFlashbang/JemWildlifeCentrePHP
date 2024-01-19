68bc2d022a8ccb5fea83a732af7f2d4fa4d03152
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-19 15:52:28 +0000

    Update animal_detail.php
    Removed unnecessary '

--- controllers/animal_detail.php
+++ controllers/animal_detail.php
@@ -1,18 +1,9 @@
-<!-- <?php
-// $heading = "Chosen Book " . $book['title'] . " with ID: " . htmlspecialchars($_GET['id']);
-// $config = require('config.php');
-// $db = new Database($config['database']);
-// $book = $db->query('select * from animals where ID = :id', ['id' => $_GET['id']])->findOrFail();
-// $returnURL = $_GET['return'];
-// require "views/book.view.php";
-?> -->
-
 <?php
 $heading = "Animal Detail". $animal['title'] . " with ID: " . htmlspecialchars($_GET['id']);
 $config = require('config.php');
 $dsn = 'mysql:' . http_build_query($config['database'], '', ';');
 $pdo = new PDO($dsn, 'root', '');
-$statement = $pdo->prepare('
+$animal = $db->query('
 SELECT
     Animal.*,
     Species_Information.Genus,
@@ -24,10 +15,8 @@ FROM
 JOIN Species_Information ON Animal.Species_ID_FK = Species_Information.Species_ID_PK
 JOIN Conservation_Status ON Species_Information.Conservation_Status_FK = Conservation_Status.Conservation_Status_PK
 WHERE
-    Animal.ID = :id
-', ['id' => $_GET['id']])->findOrFail();
-
-$statement->execute();
-$animals = $statement->fetchAll(PDO::FETCH_ASSOC);
+    Animal.ID = :id', 
+['id' => $_GET['id']]
+)->findOrFail();
 require "views/animal.view.php";
 ?>
