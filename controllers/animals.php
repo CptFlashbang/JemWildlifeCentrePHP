25df6bd4e40264a5072c0121d71e9e787294a0b1
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-19 15:16:58 +0000

    Improved SQL readability

--- controllers/animals.php
+++ controllers/animals.php
@@ -5,17 +5,15 @@ $dsn = 'mysql:' . http_build_query($config['database'], '', ';');
 $pdo = new PDO($dsn, 'root', '');
 $statement = $pdo->prepare("
 SELECT
-    A.*,
-    S.Genus,
-    S.Species,
-    S.Common_Name,
-    CS.Conservation_Status_Name
+    Animal.*,
+    Species_Information.Genus,
+    Species_Information.Species,
+    Species_Information.Common_Name,
+    Conservation_Status.Conservation_Status_Name
 FROM
     Animal A
-JOIN
-    Species_Information S ON A.Species_ID_FK = S.Species_ID_PK
-JOIN
-    Conservation_Status CS ON S.Conservation_Status_FK = CS.Conservation_Status_PK
+JOIN Species_Information ON A.Species_ID_FK = S.Species_ID_PK
+JOIN Conservation_Status ON S.Conservation_Status_FK = CS.Conservation_Status_PK
 ");
 $statement->execute();
 $animals = $statement->fetchAll(PDO::FETCH_ASSOC);
