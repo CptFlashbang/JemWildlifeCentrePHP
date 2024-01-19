acf537fd90401b182a359172dc153ac1ad29d289
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-19 11:18:04 +0000

    Added base format to animal_detail

---
+++ views/animal_detail.php
@@ -0,0 +1,15 @@
+<?php
+require('partials/head.php');
+require('partials/header.php');
+?>
+<main class="flex-grow">
+
+    <?php    
+    require('partials/banner.php');
+    ?>
+</main>
+<?php
+require('partials/footer.php');
+?>
+
+
