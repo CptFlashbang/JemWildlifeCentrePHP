1fe477bae549b66eeaad9d7df0147193add6dfff
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-19 17:19:55 +0000

    Update animal_detail.view.php

--- views/animal_detail.view.php
+++ views/animal_detail.view.php
@@ -3,7 +3,13 @@ require('partials/head.php');
 require('partials/header.php');
 ?>
 <main class="flex-grow">    
-    <p> Hello Findley </p>
+    <div class="bg-green-700">
+            <p>PICTURE</p>
+            <p><?= $animal['Animal_ID_PK'] ?></p>
+            <p><?= $animal['Name'] ?></p>
+            <p><?= $animal['Sex'] ?></p>
+            <p><?= $animal['General_Appearance'] ?></p>
+    </div>
     <?php    
     require('partials/banner.php');
     ?>
@@ -13,3 +19,9 @@ require('partials/footer.php');
 ?>
 
 
+                <a href="/JemWildlifeCentrePHP/animal_detail?id=<?= $animal['Animal_ID_PK'] ?>">
+                    <p>PICTURE</p>
+                    <p><?= $animal['Name'] ?></p>
+                    <p><?= $animal['Sex'] ?></p>
+                    <p>General_Appearance</p>
+                </a>
