60d081b13b1039ec59aaa250a66c27bee373e686
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-19 22:00:15 +0000

    Update index.php
    Perform a query to retrieve the animal details where they match the id

--- controllers/index.php
+++ controllers/index.php
@@ -19,4 +19,25 @@ for ($i = 0; $i < 3; $i++) {
     $randomNumbers[] = rand($min, $max);
 }
 
+foreach ($randomNumbers as $randomNumber) {
+    $animal = $db->query('
+        SELECT
+            Animal.*,
+            Species_Information.Genus,
+            Species_Information.Species,
+            Species_Information.Common_Name,
+            Conservation_Status.Conservation_Status_Name
+        FROM
+            Animal
+        JOIN Species_Information ON Animal.Species_ID_FK = Species_Information.Species_ID_PK
+        JOIN Conservation_Status ON Species_Information.Conservation_Status_FK = Conservation_Status.Conservation_Status_PK
+        WHERE
+            Animal.Animal_ID_PK = :id', 
+        ['id' => $randomNumber]
+    )->findOrFail();
+
+    $animals[] = $animal;
+}
+
+
 require "views/index.view.php";
