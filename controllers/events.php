fd6d25da70268aa57cab5483afda039918295fd5
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-20 15:45:25 +0000

    Update events.php
    Added sql to retrieve all events and details.
    Code repurposed from animals

--- controllers/events.php
+++ controllers/events.php
@@ -1,3 +1,16 @@
 <?php
 $heading = "Events";
+$config = require('config.php');
+$dsn = 'mysql:' . http_build_query($config['database'], '', ';');
+$pdo = new PDO($dsn, 'root', '');
+$statement = $pdo->prepare("
+SELECT
+    *
+FROM
+    Event
+");
+$statement->execute();
+$events = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
 require "views/events.view.php";
