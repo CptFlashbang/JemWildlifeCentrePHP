c90509fc20a4f920bf5c664ccbb8e2e27d987a2c
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-20 23:48:35 +0000

    Update events.php
    Improved formatting in query

--- controllers/events.php
+++ controllers/events.php
@@ -5,9 +5,19 @@ $dsn = 'mysql:' . http_build_query($config['database'], '', ';');
 $pdo = new PDO($dsn, 'root', '');
 $statement = $pdo->prepare("
 SELECT
-    *
+    Event_Title,
+    Event_Description,
+    DATE_FORMAT(Event_Date, '%d-%m-%Y') AS Formatted_Event_Date,
+    TIME_FORMAT(Start_Time, '%H:%i') AS Formatted_Start_Time,
+    TIME_FORMAT(End_Time, '%H:%i') AS Formatted_End_Time,
+    CASE Age_Rating_Fk
+        WHEN 1 THEN 'Adults'
+        WHEN 2 THEN 'Family'
+        WHEN 3 THEN 'Children'
+    END AS Age_Rating,
+    Event_Notes
 FROM
-    Event
+    Event;
 ");
 $statement->execute();
 $events = $statement->fetchAll(PDO::FETCH_ASSOC);
