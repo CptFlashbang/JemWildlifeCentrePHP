0d9a917cf32a63a74607a169252b603128d2c5c4
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-20 15:48:34 +0000

    Update events.view.php
    Added master list of events

--- views/events.view.php
+++ views/events.view.php
@@ -6,6 +6,16 @@ require('partials/header.php');
     <div class="mx-auto max-w-5xl py-6 px-8">
         <p>Hello. Welcome to the events page.</p>
     </div>
+    <div class="grid grid-cols-3 gap-4">
+        <?php foreach ($events as $event): ?>
+            <div class="bg-green-700 p-4">
+                <a href="/JemWildlifeCentrePHP/event_detail?id=<?= $event['Event_ID_PK'] ?>">
+                    <p>PICTURE</p>
+                    <p><?= $event['Name'] ?></p>
+                </a>
+            </div>
+        <?php endforeach; ?>
+    </div>
     <?php    
     require('partials/banner.php');
     ?>
