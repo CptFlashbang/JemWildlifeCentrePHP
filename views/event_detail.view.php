b35e252f013dd95c61e879a5d1aaabfd48ad7887
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-22 03:20:02 +0000

    Included more detail in event detail

--- views/event_detail.view.php
+++ views/event_detail.view.php
@@ -5,8 +5,12 @@ require('partials/header.php');
 <main class="flex-grow">    
     <div class="">
             <img src="<?= $event['image_path'] ?>" alt="<?= $event['Event_Title'] ?>">
-            <p><?= $event['Event_ID_PK'] ?></p>
             <p><?= $event['Event_Title'] ?></p>
+            <p><?= $event['Event_Description'] ?></p>
+            <p>Date: <?= $event['Event_Date'] ?></p>
+            <p>Start_Time:  <?= $event['Start_Time'] ?></p>
+            <p>End Time:  <?= $event['End_Time'] ?></p>
+            <p><?= $event['Event_Notes'] ?></p>
     </div>
     <?php    
     require('partials/banner.php');
